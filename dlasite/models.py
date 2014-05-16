4d4f875d23a5987307d485aba89091d7efdd2074
Richard <richard@koalinixdos.(none)>
2014-05-16 06:37:15 -1000

    model stubs in but not completed

--- dlasite/models.py
+++ dlasite/models.py
@@ -1,2 +1,28 @@
 from django.db import models
 
+
+class Collection(models.Model):
+
+    def __unicode__(self):
+        return self.title
+
+    def get_absolute_url(self):
+        return reverse('', args=[str(self.id)])
+
+
+class Record(models.Model):
+
+    def __unicode__(self):
+        return self.title
+
+    def get_absolute_url(self):
+        return reverse('', args=[str(self.id)])
+
+
+class Element(models.Model):
+	
+    def __unicode__(self):
+        return self.title
+
+    def get_absolute_url(self):
+        return reverse('', args=[str(self.id)])
