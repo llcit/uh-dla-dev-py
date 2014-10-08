93715dcdb63979ccc09670e413de7a0885894b6e
Richard Medina <rmedina@Ruby.local>
2014-10-07 14:25:12 -1000

    fabfile committed this without comments.

--- dlaproject/settings/base.py
+++ dlaproject/settings/base.py
@@ -1,11 +1,5 @@
 """
 Django settings for dlaproject project.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/1.6/topics/settings/
-
-For the full list of settings and their values, see
-https://docs.djangoproject.com/en/1.6/ref/settings/
 """
 
 import os
